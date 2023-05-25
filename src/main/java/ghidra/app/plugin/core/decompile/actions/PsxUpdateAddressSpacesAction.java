09f08fe3341d681cc833c172a7d9f48d136afee3
modeco80 <lily.modeco80@protonmail.ch>
2023-05-24 21:40:02 -0400

    Ghidra 10.3 support
    Some of the code came from earlier Ghidra commits before the code was removed.
    
    CSS was removed as 10.3 (possibly earlier) ships a shared copy of the CSS used for help (so we don't need it in tree anymore)

--- src/main/java/ghidra/app/plugin/core/decompile/actions/PsxUpdateAddressSpacesAction.java
+++ src/main/java/ghidra/app/plugin/core/decompile/actions/PsxUpdateAddressSpacesAction.java
@@ -12,12 +12,22 @@ import ghidra.app.decompiler.ClangFieldToken;
 import ghidra.app.decompiler.ClangFuncNameToken;
 import ghidra.app.decompiler.ClangReturnType;
 import ghidra.app.decompiler.ClangToken;
+import ghidra.app.decompiler.ClangVariableToken;
 import ghidra.app.plugin.core.decompile.DecompilerActionContext;
 import ghidra.app.plugin.core.decompile.DecompilerProvider;
 import ghidra.framework.plugintool.PluginTool;
 import ghidra.program.model.address.Address;
+import ghidra.program.model.listing.Program;
 import ghidra.program.model.listing.Function;
+import ghidra.program.model.pcode.LocalSymbolMap;
+import ghidra.program.model.pcode.GlobalSymbolMap;
+import ghidra.program.model.pcode.PcodeOp;
+import ghidra.program.model.pcode.Varnode;
 import ghidra.program.model.pcode.HighSymbol;
+import ghidra.program.model.pcode.HighFunction;
+import ghidra.program.model.pcode.HighVariable;
+
+import ghidra.program.model.pcode.HighFunctionDBUtil;
 import ghidra.util.UndefinedFunction;
 import ghidra.util.task.TaskLauncher;
 import psx.PsxAnalyzer;
@@ -39,6 +49,78 @@ public class PsxUpdateAddressSpacesAction extends AbstractDecompilerAction {
 	private boolean isFunctionCall(ClangToken tokenAtCursor) {
 		return (tokenAtCursor instanceof ClangFuncNameToken);
 	}
+
+	/**
+	 * Find the HighSymbol the decompiler associates with a specific address.
+	 * @param addr is the specific address
+	 * @param highFunction is the decompiler results in which to search for the symbol
+	 * @return the matching symbol or null if no symbol exists
+	 */
+	private static HighSymbol findHighSymbol(Address addr, HighFunction highFunction) {
+		HighSymbol highSymbol = null;
+		if (addr.isStackAddress()) {
+			LocalSymbolMap lsym = highFunction.getLocalSymbolMap();
+			highSymbol = lsym.findLocal(addr, null);
+		}
+		else {
+			GlobalSymbolMap gsym = highFunction.getGlobalSymbolMap();
+			highSymbol = gsym.getSymbol(addr);
+		}
+		return highSymbol;
+	}
+
+	/**
+	 * Track down the HighSymbol associated with a particular token.  The token may be directly attached to
+	 * the symbol, or it may be a reference that needs to be looked up.
+	 * @param token is the given token
+	 * @param highFunction is the decompiler model of the function
+	 * @return the associated HighSymbol or null if one can't be found
+	 */
+	public static HighSymbol findHighSymbolFromToken(ClangToken token, HighFunction highFunction) {
+		if (highFunction == null) {
+			return null;
+		}
+		HighVariable variable = token.getHighVariable();
+		HighSymbol highSymbol = null;
+		if (variable == null) {
+			// Token may be from a variable reference, in which case we have to dig to find the actual symbol
+			Function function = highFunction.getFunction();
+			if (function == null) {
+				return null;
+			}
+			Address storageAddress = getStorageAddress(token, function.getProgram());
+			if (storageAddress == null) {
+				return null;
+			}
+			highSymbol = findHighSymbol(storageAddress, highFunction);
+		}
+		else {
+			highSymbol = variable.getSymbol();
+		}
+		return highSymbol;
+	}
+
+	/**
+	 * Get the storage address of the variable attached to the given token, if any.
+	 * The variable may be directly referenced by the token, or indirectly referenced as a point.
+	 * @param tokenAtCursor is the given token
+	 * @param program is the Program
+	 * @return the storage Address or null if there is no variable attached
+	 */
+	private static Address getStorageAddress(ClangToken tokenAtCursor, Program program) {
+		Varnode vnode = tokenAtCursor.getVarnode();
+		Address storageAddress = null;
+		if (vnode != null) {
+			storageAddress = vnode.getAddress();
+		}
+		// op could be a PTRSUB, need to dig it out...
+		else if (tokenAtCursor instanceof ClangVariableToken) {
+			PcodeOp op = ((ClangVariableToken) tokenAtCursor).getPcodeOp();
+			storageAddress = HighFunctionDBUtil.getSpacebaseReferenceAddress(program.getAddressFactory(), op);
+		}
+		return storageAddress;
+	}
+
 
 	@Override
 	protected boolean isEnabledForDecompilerContext(DecompilerActionContext context) {
